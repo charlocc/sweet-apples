c7df7337d57988ba758cb3514d19f89162dc075c
Charlotte Clark <charlocc@usc.edu>
2021-09-28 11:04:59 -0700

    Add review finalized

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,20 +1,20 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
+  var vidId = document.location.pathname
+  vidId = vidId.split('/')
+  vidId = vidId[2];
 
-  
-  const description = document.querySelector('#review-desc').value.trim();
-
-  if (description) {
+  const review_description = document.querySelector('#review-desc').value.trim();
+  if (review_description) {
     const response = await fetch(`/api/reviews`, {
       method: 'POST',
-      body: JSON.stringify({ description }),
+      body: JSON.stringify({ videogame_id:vidId, review_description }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
-
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.reload();
     } else {
       alert('Failed to create review');
     }
